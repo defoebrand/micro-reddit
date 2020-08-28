14091cae259b10e0b888a4e5aa6394b8990d566b
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-08-28 20:28:56 +0800

    Add migration to posts to include user_id column

--- db/migrate/20200828094253_create_posts.rb
+++ db/migrate/20200828094253_create_posts.rb
@@ -1,11 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
-    create_table :posts do |t|
-      #t.belongs_to :user, index: { unique: true }, foreign_key: true
+    create_table :posts do |t|      
       t.string :title
       t.text :body
-      t.references :user, null: false, foreign_key: true
-
+      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_28_094308) do
+ActiveRecord::Schema.define(version: 2020_08_28_122135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_08_28_094308) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
