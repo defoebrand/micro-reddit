2b65ab09c7e9bdb7e0f5e9c90ad10f92e631c64d
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-08-28 20:06:51 +0800

    Edits to post migration

--- db/migrate/20200828094253_create_posts.rb
+++ db/migrate/20200828094253_create_posts.rb
@@ -1,9 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.belongs_to :user, index: { unique: true }, foreign_key: true
+      #t.belongs_to :user, index: { unique: true }, foreign_key: true
       t.string :title
       t.text :body
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
